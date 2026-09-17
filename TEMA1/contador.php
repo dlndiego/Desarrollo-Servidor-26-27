5c6bb92a9727e3f13bf107fd7601d4b9ea507528
dlndiego <diegohdeleon@gmail.com>
2026-09-17 11:22:11 +0200

    17/09/26

--- TEMA1/contador.php
+++ TEMA1/contador.php
@@ -1,20 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/info_basica.css">
 </head>
 <body>
     <?php
-        $cont1 = 1000;
+        $cont1 = 100;
         $cont2 = 10;
     ?>
 
     <h1>Probando bucles:</h1>
-    Vamos a hacer un contador del 0 a el 100 usando un FOR <span><?php 
-        for ($i=; $i <  ; $i++) { 
-            # code...
+    <p>Vamos a hacer un contador del 0 a el 100 usando un FOR </p>
+    <span><?php 
+        for ($i= 1; $i <= $cont1 ; $i++) { 
+            if ($i < $cont1 ) {
+                echo $i . ", ";
+            }else{
+                echo $i . ".";
+            }
         }
-    
     ?></span>
+    
+    <p>Vamos a hacer un contador del 10 a el 0 usando un FOR </p>
+    <span><?php 
+        while ($cont2>=0) {
+            if($cont2 > 0){
+                echo $cont2 . " - ";
+            }else{
+                echo $cont2;
+            }
+            $cont2--;
+        }
+    ?></span>
+
+
 </body>
 </html>
